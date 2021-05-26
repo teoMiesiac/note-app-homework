29b42e24fcd10644eb885c8609d6f5e46cf0b127
Tadeusz Miesiac <tadeusz.miesiac@gmail.com>
2021-05-26 19:57:56 +0200

    removed unused pages

--- src/layouts/RootLayout/RootLayout.component.tsx
+++ src/layouts/RootLayout/RootLayout.component.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { DataStoreProvider } from 'store/store'
-import { Dashboard } from 'pages/Dashboard'
 import { Landing } from 'pages/Landing'
 import { Note } from 'pages/Note'
 import { theme } from 'styles/theme'
@@ -19,9 +18,6 @@ const RootLayout = (): JSX.Element => {
             </Route>
             <Route path="/note/:id">
               <Note />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
             </Route>
           </Switch>
         </Router>
